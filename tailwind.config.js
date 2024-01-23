3187b4e3c2568673edfdd80d1345d017091865da
Zakariya Oulhadj <zakariyaoulhadj01@gmail.com>
2024-01-23 08:27:43 +0000

    Made some UI changes to portfolio page.

--- tailwind.config.js
+++ tailwind.config.js
@@ -34,7 +34,8 @@ export default {
     },
     plugins: [
         require('@tailwindcss/aspect-ratio'),
-        require('@tailwindcss/typography')
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/forms')
     ],
 }
 
