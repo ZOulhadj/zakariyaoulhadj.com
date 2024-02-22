5a5f36d2dfc9be3d4aa53726dc78157c5e66c53a
Zakariya Oulhadj <zakariyaoulhadj01@gmail.com>
2024-02-22 09:04:11 +0000

    Working on asset image loading for blog posts.
    I still need to figure out how to have both relative assets but also still be
    able to use external or links for assets in blog posts.

--- src/content/config.ts
+++ src/content/config.ts
@@ -18,14 +18,14 @@ const portfolio = defineCollection({
 
 const blog = defineCollection({
     // Type-check frontmatter using a schema
-    schema: z.object({
+    schema: ({ image }) => z.object({
         title: z.string(),
         description: z.string(),
         author: z.string(),
         // Transform string to Date object
         pubDate: z.coerce.date(),
         updatedDate: z.coerce.date().optional(),
-        heroImage: z.string().optional(),
+        heroImage: image(),
         tags: z.array(z.string()),
         draft: z.boolean()
     }),
